b0e062bb76ec723a178fd5b7e2616b3b769b29f7
mesielepush <48403308+mesielepush@users.noreply.github.com>
2020-04-02 20:07:46 -0600

    likes logic done

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,17 +1,19 @@
 class LikesController < ApplicationController
     def update
         if current_user.likes.find_by_post_id(params[:post_id]) == nil
-            current_user.likes.create(post_id: params[:post_id], result: params[:vote])
+            @like = current_user.likes.create(post_id: params[:post_id], result: params[:vote])
         else
             @like  = current_user.likes.find_by_post_id(params[:post_id])
-            @like.result = params[:vote].to_i
-            @post = Post.find_by_id(params[:post_id])
-            @post.counter += params[:vote].to_i
-            @post.save
-            puts '###########################'
-            puts @post.counter
-            puts '###########################'
-            redirect_to post_show_url(id: @post.id)
         end
+        @like.result = params[:vote].to_i
+        @like.save
+        @post = Post.find_by_id(params[:post_id])
+        @post.counter += params[:vote].to_i
+        @post.save
+        puts '###########################'
+        puts @post.counter
+        puts '###########################'
+        redirect_to post_show_url(id: @post.id, like: @like.result)
+        
     end
 end
