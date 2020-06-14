720d5cacdeabe400ede95dca266b6b93c765b6c5
mesielepush <48403308+mesielepush@users.noreply.github.com>
2020-06-13 21:31:12 -0500

    index view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,24 @@ class UsersController < ApplicationController
         end
     end
     def index
-        
+        if params[:sorted] == 'time_ago'
+            @sorted = 'time_ago'
+            @posts = Post.order('created_at').reverse.first(5)
+            @post = Post.new
+        elsif params[:sorted] == 'counter'
+            @sorted = 'counter'
+            @post = Post.new
+            @posts = Post.order('counter').last(5).reverse
+        else
+            @post = Post.new
+            @posts = Post.last(5)
+        end
     end
     def update
         
     end
 
     def vote_params
-        params.require(:user_id, :current_id).permit()
+        params.require(:user_id, :current_id).permit(:sorted)
     end
 end
