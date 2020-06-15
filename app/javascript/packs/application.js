a484db8c9ee6e1300a84938ac8aeccbf558ea48d
mesielepush <48403308+mesielepush@users.noreply.github.com>
2020-06-15 18:29:56 -0500

    Update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,12 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-import '../stylesheets/application'
-import './bootstrap_custom.js'
+
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import 'bootstrap'
