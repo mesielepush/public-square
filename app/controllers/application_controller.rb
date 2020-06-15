f898de98baec0f604dab7e5ea0085789f74addf7
mesielepush <48403308+mesielepush@users.noreply.github.com>
2020-06-15 14:55:28 -0500

    last

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up) do |u|
+        u.permit(:email, :avatar,
+                :description, :background,:password, :password_confirmation)
+        end
+        devise_parameter_sanitizer.permit(:account_update) do |u|
+        u.permit(:email, :avatar,
+            :description, :background,:password, :password_confirmation)
+        end
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -20,11 +20,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
   has_many :posts
   has_many :responses
   has_many :likes
   has_many :listeners
   has_many :listenings
+
+  
 end
