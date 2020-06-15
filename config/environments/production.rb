7927c8251fc1c3f98fd4ee4695cd7eb4fde587d2
mesielepush <48403308+mesielepush@users.noreply.github.com>
2020-06-15 15:23:24 -0500

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,10 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-  config.serve_static_files = true
+  config.serve_static_assets = true
+  config.assets.digest = true
+  
+  
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
