c5b68b054b30c8d23bff15d88baf124ec2283e23
mesielepush <48403308+mesielepush@users.noreply.github.com>
2020-06-16 12:07:40 -0500

    rubocop

--- app/controllers/listeners_controller.rb
+++ app/controllers/listeners_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# This shiny device polishes bared foos
 class ListenersController < ApplicationController
   def show
     @user = User.find_by_id(params[:user_id])
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
+# This shiny device polishes bared foos
 module UsersHelper
 end
